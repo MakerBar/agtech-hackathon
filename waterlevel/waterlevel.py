829c1f0132cfea44c4e7d6aab26eb227b3951740
Jason Sendros <jsendros@gmail.com>
2016-12-11 16:09:44 -0500

    Summary: testing waterlevel pins

--- waterlevel/waterlevel.py
+++ waterlevel/waterlevel.py
@@ -8,21 +8,22 @@ SERIESRESISTOR=560
 SENSORPIN=22
  
 def main():
-    while True:
+    for pin in range(50):
         wiringpi.wiringPiSetupGpio()
-        wiringpi.pinMode(22, 0)
-        reading = wiringpi.analogRead(SENSORPIN)
+        wiringpi.pinMode(pin, 0)
+        reading = wiringpi.analogRead(pin)
        
         print("Analog reading ")
         print(reading)
        
-        # convert the value to resistance
-        reading = (1023 / reading)  - 1
-        reading = SERIESRESISTOR / reading
-        print("Sensor resistance ") 
-        print(reading)
+        if reading != 0:
+            # convert the value to resistance
+            reading = (1023 / reading)  - 1
+            reading = SERIESRESISTOR / reading
+            print("Sensor resistance ") 
+            print(reading)        
         
-        time.sleep(1)
+        #time.sleep(1)
 
 if __name__ == "__main__":
      main()
